ae67e60a30291e4a74350606439d027bc20792f4
johannvig <102874093+johannvig@users.noreply.github.com>
2022-04-04 20:39:52 +0200

    Create liaison.py

---
+++ liaison.py
@@ -0,0 +1,4 @@
+dict = {}
+dict[1] = lambda : exec(open('scripts/example n°1.py').read())
+dict[2] = lambda : exec(open('scripts/example n°2.py').read())
+dict[3] = lambda : exec(open('scripts/main.py').read())
