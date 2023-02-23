b31e69cee808c419ee8ac26c51f39d76d4725097
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-02-23 18:57:37 +0100

    Update formatted your proxy.py

--- formatted your proxy.py
+++ formatted your proxy.py
@@ -1,100 +1,76 @@
+# Import necessary libraries
 from colorama import init, Fore
 import warnings
 from time import sleep
+
+# Disable warning messages
 warnings.filterwarnings('ignore')
 
-
 def proxy():
-    
-    name_proxy_file = input(Fore.WHITE + "what is the name of your text file where you put your proxies?")
+    # Get the name of the proxy file from user input
+    name_proxy_file = input(Fore.WHITE + "What is the name of your text file where you put your proxies? ")
 
-    print(Fore.WHITE + "What is the format of your proxies?(enter your option)")
+    # Get the format of the proxies from user input
+    print(Fore.WHITE + "What is the format of your proxies? (enter your option)")
     print(Fore.WHITE + "1. ip:port:username:password")
     print(Fore.WHITE + "2. ip:port  username and password are unique")
-    print(Fore.WHITE + "Type the number:")
-    option = input()
-    
-    
-    choicefile1 = open(str(name_proxy_file) + '.txt', 'r')
-    
-    #read every lines of the text file one by one
-    choice1 = choicefile1.readlines()
+    option = input("Type the number: ")
 
-    if option == "1":
+    # Open the proxy file
+    with open(name_proxy_file + '.txt', 'r') as proxy_file:
+        # Read all lines of the file
+        proxy_list = proxy_file.readlines()
 
-        n = 0
+        # If option 1 is selected
+        if option == "1":
+            n = 0
+            while True:
+                try:
+                    # Get the next line from the proxy list
+                    proxy_line = proxy_list[n]
+                    
+                    # Split the proxy line into its components
+                    ip, port, username, password = proxy_line.strip().split(':')
+                    
+                    # Format the proxy string
+                    formatted_proxy = f"{username}:{password}@{ip}:{port}"
 
-        x = True
+                    # Write the proxy string to a file
+                    with open('generator\\formatted proxy.txt', 'a+', encoding="utf-8") as output_file:
+                        output_file.write(formatted_proxy + "\n")
 
-        while x:
-            try:
-                #choose one line of the text file
-                choice = choice1[n]
-                
-                #deconstructs all the previous strcture to recondition a new one
-                #replace ':' by a space
-                mot = choice.replace(':', ' ')
+                    n += 1
+                except IndexError:
+                    break
 
-                #separates the different parts by a space
-                séparer = mot.split(' ')
+        # If option 2 is selected
+        elif option == "2":
+            username = input(Fore.WHITE + "What is your username? ")
+            password = input(Fore.WHITE + "What is your password? ")
+            n = 0
+            while True:
+                try:
+                    # Get the next line from the proxy list
+                    proxy_line = proxy_list[n]
+                    
+                    # Split the proxy line into its components
+                    ip, port = proxy_line.strip().split(':')
+                    
+                    # Format the proxy string
+                    formatted_proxy = f"{username}:{password}@{ip}:{port}"
 
-                
-                ip = séparer[0]
+                    # Write the proxy string to a file
+                    with open('generator\\formatted proxy.txt', 'a+', encoding="utf-8") as output_file:
+                        output_file.write(formatted_proxy + "\n")
 
-                port = séparer[1]
+                    n += 1
+                except IndexError:
+                    break
 
-                username = séparer[2]
+    # Print completion messages
+    print(Fore.WHITE + "Program is over")
+    print(Fore.WHITE + "Job done. Enjoy your day!")
 
-                password = séparer[3]
-
-                proxy = username + ":" + password + "@" + ip + ":" + port
-
-                f = open('generator\\formatted proxy .txt', 'a+', encoding="utf-8") #you can change the path of the formatted proxy.py file
-                
-                #write the new proxy structure in the text file
-                f.writelines(str(proxy) + "\n")
-
-                f.close()
-
-                n += 1
-
-            except:
-                f.close()
-                x = False
-    else:
-
-        username = input(Fore.WHITE + "What is your username?")
-        
-        password = input(Fore.WHITE + "What is you password?")
-
-        n = 0
-
-        x = True
-
-        while x:
-            try:
-                choice = choice1[n]
-
-                mot = choice.replace(':', ' ')
-
-                séparer = mot.split(' ')
-
-                ip = séparer[0]
-
-                port = séparer[1]
-
-                proxy = username + ":" + password + "@" + ip + ":" + port
-
-                f = open('generator\\formatted proxy .txt', 'a+', encoding="utf-8")
-                f.writelines(str(proxy) + "\n")
-
-                n += 1
-
-            except:
-                f.close()
-                x = False
-
-
+# Call the proxy function
 proxy()
-print(Fore.WHITE + "program is over")
-print(Fore.WHITE + "Job done. Enjoy your day!")
+
